24118d0e121f3d9f93a3089d8da87412b90ccbe1
dhafer malouche <dhafer.malouche@qu.edu.qa>
2025-02-19 17:40:44 +0300

    ok

---
+++ practicesheet2.R
@@ -0,0 +1,17 @@
+# Define the numeric vector with censored values indicated by '*'
+y <- c(2, 2, 21, 21, 4, 3, 9, 38, 5, 3, 3, 20, 6, 13, 3, 31, 6, 14, 5, 22, 17, 13, 10, 9, 9)
+
+# Define the censoring indicator vector (1 for uncensored, 0 for censored)
+cens <- c(1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1)
+
+library(survival)
+
+# Kaplan-Meier estimator
+km_fit <- survfit(Surv(y, cens) ~ 1)
+summary(km_fit)
+# Nelson-Aalen estimator
+na_fit <- survfit(Surv(y, cens) ~ 1, type = "fh")  # Fleming-Harrington estimator (equivalent to NA)
+summary(na_fit)
+
+na_fit1 <- survfit(Surv(y, cens) ~ 1, type = "fh",conf.type='plain')  # Fleming-Harrington estimator (equivalent to NA)
+summary(na_fit1)
